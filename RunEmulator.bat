b2a67b729bba0772ece6a0a462ee8d40b04a7302
Raza9798 <m.raza9798@gmail.com>
2023-11-07 19:01:40 +0530

    bat file for windows has been created

---
+++ RunEmulator.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Available Android Virtual Devices (AVDs):
+
+"%USERPROFILE%\AppData\Local\Android\Sdk\emulator\emulator" -list-avds
+echo.
+
+echo.
+
+set /p avdName=Enter the name of the AVD you want to start:
+
+"%USERPROFILE%\AppData\Local\Android\Sdk\emulator\emulator" -avd %avdName%
